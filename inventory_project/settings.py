2a4a969b8596f9091af458e3a98777c619bbbf85
cyber-volk <mastervolk160@gmail.com>
2025-01-25 21:34:52 +0100

    fixed the setting for pythonanyweredeployment collecting the statics and pip freeze to requirements.txt

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'your-secret-key-replace-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'cybervolk.pythonanywhere.com']
 
 # Application definition
 INSTALLED_APPS = [
@@ -92,5 +92,6 @@ AUTH_USER_MODEL = 'inventory.User'
 
 # Additional settings for static and media files
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
